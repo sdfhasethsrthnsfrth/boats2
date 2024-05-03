614daaca3f61e4e658ca1548950e0304f2297f24
Bonsa-BE <68948638+Bonsa-BE@users.noreply.github.com>
2024-05-03 11:00:38 +0300

    Update heartbeat.py

--- project/heartbeat.py
+++ project/heartbeat.py
@@ -3,4 +3,4 @@ from send_data import *
 
 #this script repeats every 15 minutes as a cronjob
 ser = open_serial() #open serial port
-send_message(ser, 0, 0, "0x00") #send heartbeat as "0"
+send_message(ser, 0, 0, "\x00") #send heartbeat as "0"
